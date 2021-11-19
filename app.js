fb19779f09f512bcbafba848be3ec3b606ec4528
Crong (SungEun Kim) <mirxx@naver.com>
2021-11-19 19:33:49 +0900

    입력값 출현하기 (err)

--- app.js
+++ app.js
@@ -2,18 +2,30 @@
 const express = require('express') //const 는 상수이며 변수가 없이 사용을 하지 못함
 const app = express() // Express app 객체로 모든 서버의 일을 처리한다는 뜻 
 const port = 7000 // Port번호는 localhost.7000 번으로 찍힘.
+// node app.js 로컬 커맨드 명령어
 
 const toDolists = ["밥먹기"]
 
 app.set('view engine', 'pug')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req,res) => { 
     // middleware 함수에 대한 HTTP 요청 인수(req) 응답 인수 (res) 콜백 인수 (next)라고 불림
     // index.pug 에 있는 =toDoListTitle 을 가져옴 
     res.render('index', {toDoListTitle: '오늘의 할 일 : ' + toDolists.length, toDolists: toDolists})
 })
+
+app.post('/add_list', (req,res) => {
+    const newContent = req.body.content
+    console.log(newContent + '추가하기' )
+    toDolists.push(newContent)
+    res.redirect('/')
+} )
+
 // => 화살표 함수는 function 키워드 대신 사용이 가능 (간략한 방법으로 함수 선언 가능)
 // app.listen(port, () => {
-app.listen(port, function() {
+app.listen(port, () => {
     console.log('connected')
 })
